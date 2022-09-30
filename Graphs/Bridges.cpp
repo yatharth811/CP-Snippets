01a2729180c451f2f9f4953b3fa0bcfc8dc635c0
yatharth811 <yatharth.bansiwal@gmail.com>
2022-10-01 02:03:45 +0530

    Added Bridges using DO

--- Graphs/Bridges.cpp
+++ Graphs/Bridges.cpp
@@ -19,7 +19,7 @@ ll ceil(ll a, ll b) {
 }
 
 
-void dfs(ll v, vector<ll>* adj, vector<bool>& visited, vector<ll>& hps, vector<ll>& level, vector<ll>& parent){
+void dfsHighPoints(ll v, vector<ll>* adj, vector<bool>& visited, vector<ll>& hps, vector<ll>& level, vector<ll>& parent){
     visited[v] = true;
 
     for (auto x: adj[v]){
@@ -28,16 +28,47 @@ void dfs(ll v, vector<ll>* adj, vector<bool>& visited, vector<ll>& hps, vector<l
             // back edge
             if (visited[x]){
                 hps[v] = min(hps[v], level[x]);
-                // cout << "FROM DFS: " << v << " " << (hps[v]) << " "  << level[x] << endl;
             }
             // forward edge
             else{
                 level[x] = level[v] + 1;
                 parent[x] = v;
-                dfs(x, adj, visited, hps, level, parent);
+                dfsHighPoints(x, adj, visited, hps, level, parent);
                 hps[v] = min(hps[v], hps[x]);
-                // cout << "FROM DFS: " << v << " " << (hps[v]) << " "  << level[x] <<  endl;
             }
+        }
+    }
+}
+
+
+void dfsDp(ll v, vector<ll>* adj, vector<ll>& dp, vector<bool>& visited, vector<ll>& level, vector<ll>& parent){
+    visited[v] = true;
+
+    for (auto x: adj[v]){
+        if (x!=parent[v]){
+
+            if (visited[x]){
+                // Back edge going up from v
+                if (level[x] < level[v]){
+                    dp[v]++;
+                }
+                // Back edge going down from v
+                else{
+                    dp[v]--;
+                }
+            }
+            else{
+                level[x] = level[v]+1;
+                parent[x] = v;
+
+                dfsDp(x, adj, dp, visited, level, parent);
+
+                // Back edge going up from child of v
+                dp[v] += dp[x];
+
+            }
+
+
         }
     }
 }
@@ -60,26 +91,25 @@ void solve(){
     vector<ll> parent(n, -1);
     vector<ll> level(n, 0);
     vector<ll> hps(n, INT_MAX);
+    vector<ll> dp(n, 0);
 
 
     // Approach 1: DP on High Points
-    dfs(0, adj, visited, hps, level, parent);
-
-    // Approach 2: DP on Edges Only
-
-
-    // ll i = 0;
-    // for (auto x: parent){
-    //     cout << i << " " << x << " " << level[i] << " " << hps[i++] << endl;
-    // }
-    // cout << endl;
-
+    dfsHighPoints(0, adj, visited, hps, level, parent);
     rep(i,1,n){
         if (hps[i] > level[i]-1){
             cout << parent[i] << " " << i  << endl;
         }
     }
 
+    // Approach 2: DP on Edges Only
+    // dp[u] = no of back edges going up from u + no of back edges going down from u - sigma dp[v] (v -> children of u)
+    dfsDp(0, adj, dp, visited, level, parent);
+    rep(i,1,n){
+        if (!dp[i]){
+            cout << parent[i] << " " << i << endl;
+        }
+    }
 
 }
 
